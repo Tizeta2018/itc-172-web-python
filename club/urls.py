736147d31f990c3268e25f20cff8c655ccb3866f
Tizeta2018 <44073038+Tizeta2018@users.noreply.github.com>
2019-03-11 11:27:40 -0700

    doing test

--- club/urls.py
+++ club/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
 
-urlpatterns=[
-    path('', views.index, name='index'),
+
+urlpatterns =[
+    path('', views.index,name='index' ),
     path('clubmeetings/', views.clubmeetings, name='meetings' ),
     path('clubminutes/', views.clubminutes, name='minutes' ),
     path('clubresources/', views.clubresources, name='resources' ),
+    path('getmeeting/', views.getmeeting, name='getmeeting' ),
+    path('meetingdetail/<int:id>', views.meetingdetail, name='details'),
 ]
--- club/views.py
+++ club/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from.models import Meeting, MeetingMinutes, Resource, Event
+from.models import Meeting, MeetingMinutes, Resource, Event 
+from django.contrib.auth.decorators import login_required
+
+
 #create your views here.
 def index (request):
     return render(request, 'club/index.html')
@@ -19,3 +22,27 @@ def clubresources (request):
 def clubevents (request):
     eventss_list=Events.objects.all()
     return render (request, 'club/events.html',{'events_list': events_list})
+
+def getmeeting(request):
+    meetings_list=Meeting.objects.all()  
+    return render (request, 'club/meetings.html',{'meetings_list': meetings_list})
+
+def meetingdetail(request, id):
+    detail=get_object_or_404(Meeting, pk=id)  
+    context={'detail': detail}
+    return render (request, 'club/details.html',context= context)
+#form view
+@login_required
+def newResource(request):
+    form=ResourceForm
+    if request.method=='POST':
+        form=ResourceForm(request.POST)
+        if form.is_valid():
+            post=form.save(commit=True)
+            post.save()
+            form=ResourceForm()
+    else:
+        form=ResourceForm()
+    return render(request, 'club/newresource.html', {'form': form})
+
+
