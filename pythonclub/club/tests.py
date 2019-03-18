e86ca24056fdcacfb55f3e601c3bdbdef9f3db10
Tizeta2018 <44073038+Tizeta2018@users.noreply.github.com>
2019-03-18 14:56:37 -0700

    more test
    adding test

--- pythonclub/club/tests.py
+++ pythonclub/club/tests.py
@@ -21,3 +21,25 @@ class ResourceTest(TestCase):
 
     def test_tablename(self):
         self.assertEqual(str(Resource._meta.db_table), 'resource')
+
+class MeetingMinutesTest(TestCase):
+    def test_stringOutput(self):
+        minutes=MeetingMinutes(Minutestext='More Test')
+        self.assertEqual(str(minutes), minutes.minutestext)
+
+    def test_tablename(self):
+        self.assertEqual(str(MeetingMinutes._meta.db_table), 'MeetingMinutes')
+
+class EventTest(TestCase):
+    def test_stringOutput(self):
+        event=Event(eventitle='raise payment')
+        self.assertEqual(str(event), event.eventitle)
+
+    def test_tablename(self):
+        self.assertEqual(str(Event._meta.db_table), 'event')
+
+
+
+
+
+
